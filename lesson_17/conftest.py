ecb8eeec2b8b0932bd022606883237ca79813bf2
Olena96 <filchak.elena@gmail.com>
2024-01-07 22:52:49 +0200

    changed lesson 17

--- lesson_17/conftest.py
+++ lesson_17/conftest.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 
 @pytest.fixture
 def chrome():
-    driver = webdriver.Chrome(executable_path="/Users/pavlokostyshen/Desktop/hillel/Hillel_october_23/lesson_17/chromedriver")
+    driver = webdriver.Chrome(executable_path=r"C:\Users\Olena Filchak\Desktop\Python courses\lesson_17\
+    chromedriver.exe")
     yield driver
     driver.quit()
